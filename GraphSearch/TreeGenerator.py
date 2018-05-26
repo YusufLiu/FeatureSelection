a2acee2cddf3c440390b2e6021d9508cecb33cd7
Yusuf <yusuf2013dream@gmail.com>
2018-05-26 16:55:09 -0400

    update

--- GraphSearch/TreeGenerator.py
+++ GraphSearch/TreeGenerator.py
@@ -11,29 +11,32 @@ class TreeGenerator(object):
     def featureToTree(self, data):
         columns = list(data.columns.values)
         columns.remove('diagnosis')
-        columns = columns[:9]
+        #columns = columns[:10]
 
         treeRoot = ft.featureTree(data=[])
         print("Tree generating")
-        featuresTree = self.generateChild(treeRoot,columns)
+        featuresTree = self.generateChild(treeRoot,columns,[])
         #featuresTree = self.generateChild(treeRoot, ['a','b','c','d'])
 
         return featuresTree
 
-    def generateChild(self,Node,unUsedColumns):
+    def generateChild(self,Node,unUsedColumns,usedSets):
         children = []
         #print(unUsedColumns)
         for c in unUsedColumns:
             data = Node.data + [c]
             #print(data)
-            childNode = ft.featureTree(data=data)
-            temp = unUsedColumns[:] #cant use tem =unUsedColumns,lazy python just give temp the pointer to original data
-            temp.remove(c)
-            if(temp):
-                childs = self.generateChild(childNode,temp)
-                children.append(childs)
-            #print(childs.children)
-            #print(len(children))
+            data.sort()
+            if(data not in usedSets):
+                usedSets.append(data)
+                childNode = ft.featureTree(data=data)
+                temp = unUsedColumns[:] #cant use tem =unUsedColumns,lazy python just give temp the pointer to original data
+                temp.remove(c)
+                if(temp):
+                    childs = self.generateChild(childNode,temp,usedSets)
+                    children.append(childs)
+                #print(childs.children)
+                #print(len(children))
 
 
         Node.children = children
